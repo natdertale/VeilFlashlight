5665a7ad85b971c347c205578f91e292595444c0
natdertale <78158781+natdertale@users.noreply.github.com>
2025-03-06 17:38:17 +0100

    multiplayer support

--- src/client/java/natdertale/flashlights/FlashLightsClient.java
+++ src/client/java/natdertale/flashlights/FlashLightsClient.java
@@ -1,10 +1,16 @@
 package natdertale.flashlights;
 
+
+import natdertale.flashlights.light.ModLight;
 import net.fabricmc.api.ClientModInitializer;
+
+import java.util.function.Supplier;
 
 public class FlashLightsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		ModLight.init();
 	}
 }
